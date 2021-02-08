fc3b4eb5b8e9ec7b8e0634ebad130b4bb669cc82
evayl2 <evayl2@illinois.edu>
2021-02-08 15:48:31 -0600

    helena's twitter code

--- twitter-json.py
+++ twitter-json.py
@@ -1,8 +1,35 @@
 import requests
-import twitterconfig
-response = requests.get(
-    'https://api.twitter.com/1.1/search/tweets.json?q=tesla',
-    headers={
-        'authorization': 'Bearer '+twitterconfig.twitter["BEARER_TOKEN"]
-})
-print(response.json())
+from twitterconfig import *
+import pandas as pd
+import tweepy
+
+
+# Store tweets data in a dataframe
+def tweets_df(results):
+    id_list = [tweet.id for tweet in results]
+    data_set = pd.DataFrame(id_list, columns=["id"])
+    data_set["text"] = [tweet.text for tweet in results]
+    data_set["Hashtags"] = [tweet.entities.get('hashtags') for tweet in results]
+
+    filename = 'scraped_tweets.csv'
+    # we will save our database as a CSV file.
+    data_set.to_csv(filename)
+    return data_set
+
+
+if _name_ == '_main_':
+
+    auth = tweepy.OAuthHandler(twitter["API_KEY"], twitter["API_KEY_SECRET"])  # Interacting with twitter's API
+    auth.set_access_token(twitter["ACCESS_TOKEN"], twitter["ACCESS_TOKEN_SECRET"])
+    api = tweepy.API(auth, wait_on_rate_limit=True)  # creating the API object
+
+    # Enter Hashtag and initial date
+    print("Enter Twitter HashTag to search for")
+    words = input()
+
+    # Extracting Tweets
+    results = []
+    for tweet in tweepy.Cursor(api.search, q=words, lang="en").items(5):
+        results.append(tweet)
+
+    data_set = tweets_df(results)
