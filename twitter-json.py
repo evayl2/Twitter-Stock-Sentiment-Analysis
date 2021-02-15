e0be0be7b489d18968b52477e39b756d3dd43aa4
evayl2 <evayl2@illinois.edu>
2021-02-15 16:35:18 -0600

    fixed hashtag parsing

--- twitter-json.py
+++ twitter-json.py
@@ -2,7 +2,16 @@ import requests
 from twitterconfig import *
 import pandas as pd
 import tweepy
-
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+# import seaborn as sns
+import string
+import nltk
+from nltk.stem.porter import *
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+from wordcloud import WordCloud
 
 
 # Store tweets data in a dataframe
@@ -10,9 +19,9 @@ def tweets_df(results):
     id_list = [tweet.id for tweet in results]
     data_set = pd.DataFrame(id_list, columns=["id"])
     data_set["text"] = [tweet.text for tweet in results]
-    data_set["Hashtags"] = [tweet.entities.get('hashtags') for tweet in results]
+    data_set["Hashtags"] = [tweet.entities['hashtags'] for tweet in results]
 
-    filename = 'scraped_tweets.csv'
+    filename = 'tweet_data/hashtag_scraped_tweets.csv'
     # we will save our database as a CSV file.
     data_set.to_csv(filename)
     return data_set
@@ -26,7 +35,7 @@ if __name__ == '__main__':
 
     # Enter Hashtag and initial date
     print("Enter Twitter HashTag to search for")
-    words = input()
+    words = "#"+input()
 
     # Extracting Tweets
     results = []
@@ -43,7 +52,7 @@ def get_tweets_from_user(results):
     data_set["User"] = [tweet.user.screen_name for tweet in results]
 
 
-    filename = 'scraped_user_tweets.csv'
+    filename = 'tweet_data/scraped_user_tweets.csv'
     # we will save our database as a CSV file.
     data_set.to_csv(filename)
     return data_set
@@ -59,7 +68,45 @@ if __name__ == '__main__':
     words = input()
 
     results = []
-    for tweet in tweepy.Cursor(api.user_timeline,id=words).items(5):
+    for tweet in tweepy.Cursor(api.user_timeline,id=words).items(50):
         results.append(tweet)
 
     data_set = get_tweets_from_user(results)
+
+
+# Tweet parsing code from https://www.analyticsvidhya.com/blog/2018/07/hands-on-sentiment-analysis-dataset-python/
+
+def remove_pattern(input_txt, pattern):
+    r = re.findall(pattern, input_txt)
+    for i in r:
+        input_txt = re.sub(i, '', input_txt)
+
+    return input_txt
+
+data = pd.read_csv('tweet_data/scraped_user_tweets.csv')
+urls = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+data["text"] = np.vectorize(remove_pattern)(data['text'], urls)
+data["text"] = np.vectorize(remove_pattern)(data['text'], "@[\w]*")
+data["text"] = data['text'].str.replace("[^a-zA-Z#]", " ")
+data["text"]= data["text"].apply(lambda x: ' '.join([w for w in x.split() if len(w)>2]))
+
+tokenized_tweet = data["text"] .apply(lambda x: x.split())
+stemmer = PorterStemmer()
+tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x]) # stemming
+
+for i in range(len(tokenized_tweet)):
+    tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
+
+data['text'] = tokenized_tweet
+data.to_csv(index=False, path_or_buf="tweet_data/cleaned_user_tweet.csv")
+
+all_words = ' '.join([text for text in data['text']])
+
+
+wordcloud = WordCloud(width=800, height=500, random_state=21, max_font_size=110).generate(all_words)
+
+plt.figure(figsize=(10, 7))
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis('off')
+# plt.show()
+plt.savefig("tweet_data/elonmusk_tweets.png", bbox_inches='tight')
